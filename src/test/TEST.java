73ca24de4ae90401713c11a1d9ce503ad3e0a7a7
OpitevAD <OpitevAD@3022-OAD.iackvno.local>
2017-03-24 15:38:36 +0300

    Тестинг

---
+++ src/test/TEST.java
@@ -0,0 +1,21 @@
+
+package test;
+
+
+
+public class TEST {
+
+    public static void main(String[] args) {
+        double GM = 3.986004418E14;  
+        double omega = 7.292115E-5;
+        double a = 6378137.0;
+        double C20norm = -0.000484166774985001;
+        double alfa = 0.00335281066475;
+        double ro = 206264.80625;
+        double alfa2 = 
+        
+
+        System.out.println(omega);
+    }
+    
+}
